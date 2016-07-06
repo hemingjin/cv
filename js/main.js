368ae79cec83251f25119cb030c074a0c34832b8
hemingjin <lp877709145@163.com>
2016-07-06 16:27:37 +0800

    1627

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+	//侧栏遮罩
+	// $(".sidebar").mouseover(function(){
+	// 	$(".sidebar .bg-over").animate({"display":"block"},"slow");
+	// }).mouseleave(function(){
+	// 	$(".sidebar .bg-over").animate({"display":"block"},"block");;
+	// });
+
+});
